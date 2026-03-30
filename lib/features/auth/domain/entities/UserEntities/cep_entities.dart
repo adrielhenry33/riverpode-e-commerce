616f99393b9c6994697304186a6747a63c215517
Andriel Henry Dias Barbosa <estag202@wetzel.net>
2026-03-30 16:20:41 -0300

    refactor: cria as camadas core e features

---
+++ lib/features/auth/domain/entities/UserEntities/cep_entities.dart
@@ -0,0 +1,31 @@
+class CepModel {
+  final String cep;
+  final String logradouro;
+  final String complemento;
+  final String bairro;
+  final String localidade;
+  final String uf;
+  final String estado;
+
+  CepModel({
+    required this.bairro,
+    required this.cep,
+    required this.logradouro,
+    required this.complemento,
+    required this.localidade,
+    required this.uf,
+    required this.estado,
+  });
+
+  factory CepModel.fromJson(Map<String, dynamic> json) {
+    return CepModel(
+      bairro: json['bairro'],
+      cep: json['cep'],
+      logradouro: json['logradouro'],
+      complemento: json['complemento'],
+      localidade: json['localidade'],
+      uf: json['uf'],
+      estado: json['estado'],
+    );
+  }
+}
